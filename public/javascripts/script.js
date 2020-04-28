41342ffff0d62621f89765169ca0ebc22e279bfc
Alan Klein <alan@alanklein.dev>
2020-04-27 20:51:25 -0400

    Fix Legend Box Width

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -17,24 +17,27 @@ class VoteChart {
         labels: ['Cake', 'Pie'],
         datasets: [
           {
-            label: 'Count',
+            label: '# of Votes',
+
             data: this.chartData,
             backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
               'rgba(54, 162, 235, 0.2)',
               'rgba(255, 206, 86, 0.2)',
             ],
-            borderColor: [
-              'rgba(255, 99, 132, 1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-            ],
+            borderColor: ['rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)'],
             borderWidth: 1,
             barPercentage: 0.4,
           },
         ],
       },
       options: {
+        legend: {
+          labels: {
+            fontColor: 'black',
+            fontSize: 18,
+            boxWidth: 0,
+          },
+        },
         scales: {
           yAxes: [
             {
